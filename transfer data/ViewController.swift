81d5be488dfe1c7175c7d2cb84b04bb8cecc4c9c
Shien <f0928652413@gmail.com>
2022-07-28 13:01:24 +0800

    Initial Commit

---
+++ transfer data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  transfer data
+//
+//  Created by Shien on 2022/7/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
